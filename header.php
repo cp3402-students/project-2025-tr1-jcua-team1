cae7d09263d0c18101f334754db9ec7c240549e3
Joshua Delos Santos <127062493+JoshuaDelosSantos@users.noreply.github.com>
2025-03-20 19:19:05 +1000

    Comment out code that displays site title

--- header.php
+++ header.php
@@ -29,15 +29,19 @@
 		<div class="site-branding">
 			<?php
 			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
+			/*
+				*This is an original code that displays the site title.
+
+						if ( is_front_page() && is_home() ) :
+							?>
+							<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+							<?php
+						else :
+							?>
+							<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+							<?php
+						endif;
+			*/
 			$team1theme_description = get_bloginfo( 'description', 'display' );
 			if ( $team1theme_description || is_customize_preview() ) :
 				?>
