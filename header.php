23d98e4292582c8ad3bbf212c320ae4861e319a8
Joshua Delos Santos <127062493+JoshuaDelosSantos@users.noreply.github.com>
2025-03-24 17:21:52 +1000

    Add placeholder for when no logo is present

--- header.php
+++ header.php
@@ -28,7 +28,19 @@
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
 			<?php
-			the_custom_logo();
+			if (has_custom_logo()) {
+				the_custom_logo();
+			} else {
+				// Add a placeholder with same height as the logo would have
+				?>
+				<div class="logo-placeholder">
+					<?php if (is_front_page() && is_home()) : ?>
+						<h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
+					<?php else : ?>
+						<p class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></p>
+					<?php endif; ?>
+				</div>
+			<?php } 
 			/*
 				*This is an original code that displays the site title.
 
