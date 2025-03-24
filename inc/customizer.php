4e0449f97e73e7ecf28ff14e3dd01f223b7e801f
Joshua Delos Santos <127062493+JoshuaDelosSantos@users.noreply.github.com>
2025-03-24 17:18:32 +1000

    Add header height to customiser

--- inc/customizer.php
+++ inc/customizer.php
@@ -59,3 +59,57 @@ function team1theme_customize_preview_js() {
 	wp_enqueue_script( 'team1theme-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), _S_VERSION, true );
 }
 add_action( 'customize_preview_init', 'team1theme_customize_preview_js' );
+
+/**
+ * Add logo size controls to the customizer
+ */
+function team1theme_logo_customize_register($wp_customize) {
+    // Header Section
+    $wp_customize->add_section('team1theme_logo_options', array(
+        'title'       => __('Logo Options', 'team1theme'),
+        'priority'    => 30,
+        'description' => __('Adjust your logo and header sizing', 'team1theme'),
+    ));
+    
+    // Header Height Setting
+    $wp_customize->add_setting('team1theme_header_height', array(
+        'default'           => 100,
+        'sanitize_callback' => 'absint',
+        'transport'         => 'refresh',
+    ));
+    
+    // Header Height Control
+    $wp_customize->add_control('team1theme_header_height', array(
+        'label'       => __('Header Height (px)', 'team1theme'),
+        'section'     => 'team1theme_logo_options',
+        'type'        => 'number',
+        'input_attrs' => array(
+            'min'  => 50,
+            'max'  => 250,
+            'step' => 10,
+        ),
+    ));
+}
+add_action('customize_register', 'team1theme_logo_customize_register');
+
+/**
+ * Apply customizer header height to CSS
+ */
+function team1theme_logo_customize_css() {
+    $header_height = get_theme_mod('team1theme_header_height', 100);
+    
+    $css = "
+        .site-header {
+            min-height: {$header_height}px;
+        }
+        .site-branding {
+            min-height: {$header_height}px;
+        }
+        .custom-logo {
+            max-height: {$header_height}px;
+        }
+    ";
+    
+    wp_add_inline_style('team1theme-style', $css);
+}
+add_action('wp_enqueue_scripts', 'team1theme_logo_customize_css', 20);
