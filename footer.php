d5e58a6bde3b3a3242df849ff54387adf12b9c07
Joshua Delos Santos <127062493+JoshuaDelosSantos@users.noreply.github.com>
2025-03-30 22:06:21 +1000

    Update footer.php to enhance footer customisation with dynamic background and text colors

--- footer.php
+++ footer.php
@@ -11,19 +11,16 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
+	<footer id="colophon" class="site-footer" style="background-color: <?php echo esc_attr(get_theme_mod('footer_bg_color', '#f8f9fa')); ?>; color: <?php echo esc_attr(get_theme_mod('footer_text_color', '#333333')); ?>;">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'team1theme' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'team1theme' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'team1theme' ), 'team1theme', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
+			<?php echo wp_kses_post(get_theme_mod('footer_text', 'Copyright © ' . date('Y') . ' ' . get_bloginfo('name'))); ?>
+			
+			<?php if (get_theme_mod('show_wordpress_credit', true)) : ?>
+				<span class="sep"> | </span>
+				<a href="<?php echo esc_url(__('https://wordpress.org/', 'team1theme')); ?>">
+					<?php printf(esc_html__('Proudly powered by %s', 'team1theme'), 'WordPress'); ?>
+				</a>
+			<?php endif; ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
