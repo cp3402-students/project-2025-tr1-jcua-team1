40738681069068192245f6ba3ab612e139ba1e79
Joshua Delos Santos <127062493+JoshuaDelosSantos@users.noreply.github.com>
2025-03-31 13:23:49 +1000

    Refactor page template with layout options and featured image display

--- page.php
+++ page.php
@@ -2,37 +2,53 @@
 /**
  * The template for displaying all pages
  *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site may use a
- * different template.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
  * @package Team1Theme
  */
 
 get_header();
+
+// Get page layout setting
+$page_layout = get_theme_mod('page_layout', 'with-sidebar');
 ?>
 
-	<main id="primary" class="site-main">
+<main id="primary" class="site-main <?php echo esc_attr($page_layout); ?>">
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+    <?php
+    // Display featured image if set to banner mode
+    $featured_image_display = get_theme_mod('page_featured_image_display', 'banner');
+    if ($featured_image_display === 'banner' && has_post_thumbnail()) :
+    ?>
+        <div class="page-featured-image-banner">
+            <?php the_post_thumbnail('full'); ?>
+        </div>
+    <?php endif; ?>
 
-			get_template_part( 'template-parts/content', 'page' );
+    <?php
+    while (have_posts()) :
+        the_post();
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+        // Featured image above content
+        if ($featured_image_display === 'above' && has_post_thumbnail()) :
+            echo '<div class="page-featured-image-above">';
+            the_post_thumbnail('large');
+            echo '</div>';
+        endif;
 
-		endwhile; // End of the loop.
-		?>
+        get_template_part('template-parts/content', 'page');
 
-	</main><!-- #main -->
+        // If comments are open or we have at least one comment, load up the comment template.
+        if (comments_open() || get_comments_number()) :
+            comments_template();
+        endif;
+
+    endwhile; // End of the loop.
+    ?>
+
+</main><!-- #main -->
 
 <?php
-get_sidebar();
+// Only include sidebar for the with-sidebar layout
+if ($page_layout === 'with-sidebar') {
+    get_sidebar();
+}
 get_footer();
