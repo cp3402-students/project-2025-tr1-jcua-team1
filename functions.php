e1019c99be1e00f0531db24e30d9692ed921dff1
Joshua Delos Santos <127062493+JoshuaDelosSantos@users.noreply.github.com>
2025-03-31 13:28:10 +1000

    Add page-specific meta boxes for overriding theme defaults

--- functions.php
+++ functions.php
@@ -448,6 +448,11 @@ require get_template_directory() . '/inc/customizer.php';
 require get_template_directory() . '/inc/page-customizer.php';
 
 /**
+ * Page-specific meta boxes for overriding theme defaults.
+ */
+require get_template_directory() . '/inc/page-meta-boxes.php';
+
+/**
  * Load Jetpack compatibility file.
  */
 if ( defined( 'JETPACK__VERSION' ) ) {
