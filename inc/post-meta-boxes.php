4ee649edf5577c4b088ab7edcb579ac6564f6aa6
Joshua Delos Santos <127062493+JoshuaDelosSantos@users.noreply.github.com>
2025-03-31 19:06:49 +1000

    Add heading and text alignment options to post meta settings

--- inc/post-meta-boxes.php
+++ inc/post-meta-boxes.php
@@ -33,6 +33,22 @@ function team1theme_post_settings_callback($post) {
     if ($show_post_meta === '') {
         $show_post_meta = 'default'; // Use default from customizer
     }
+
+    // Get saved heading alignment value
+    $heading_alignment = get_post_meta($post->ID, '_team1theme_heading_alignment', true);
+
+    // Set default heading alignment if empty
+    if ($heading_alignment === '') {
+        $heading_alignment = 'default';
+    }
+
+    // Get saved text alignment value
+    $text_alignment = get_post_meta($post->ID, '_team1theme_text_alignment', true);
+
+    // Set default text alignment if empty
+    if ($text_alignment === '') {
+        $text_alignment = 'default';
+    }
     ?>
     <p>
         <label for="show_post_meta"><?php _e('Show Post Meta Information:', 'team1theme'); ?></label>
@@ -40,6 +56,25 @@ function team1theme_post_settings_callback($post) {
             <option value="default" <?php selected($show_post_meta, 'default'); ?>><?php _e('Theme Default', 'team1theme'); ?></option>
             <option value="yes" <?php selected($show_post_meta, 'yes'); ?>><?php _e('Yes', 'team1theme'); ?></option>
             <option value="no" <?php selected($show_post_meta, 'no'); ?>><?php _e('No', 'team1theme'); ?></option>
+        </select>
+    </p>
+    <p>
+        <label for="heading_alignment"><?php _e('Heading Alignment:', 'team1theme'); ?></label>
+        <select name="heading_alignment" id="heading_alignment">
+            <option value="default" <?php selected($heading_alignment, 'default'); ?>><?php _e('Theme Default', 'team1theme'); ?></option>
+            <option value="left" <?php selected($heading_alignment, 'left'); ?>><?php _e('Left', 'team1theme'); ?></option>
+            <option value="center" <?php selected($heading_alignment, 'center'); ?>><?php _e('Center', 'team1theme'); ?></option>
+            <option value="right" <?php selected($heading_alignment, 'right'); ?>><?php _e('Right', 'team1theme'); ?></option>
+        </select>
+    </p>
+    <p>
+        <label for="text_alignment"><?php _e('Text Alignment:', 'team1theme'); ?></label>
+        <select name="text_alignment" id="text_alignment">
+            <option value="default" <?php selected($text_alignment, 'default'); ?>><?php _e('Theme Default', 'team1theme'); ?></option>
+            <option value="left" <?php selected($text_alignment, 'left'); ?>><?php _e('Left', 'team1theme'); ?></option>
+            <option value="center" <?php selected($text_alignment, 'center'); ?>><?php _e('Center', 'team1theme'); ?></option>
+            <option value="right" <?php selected($text_alignment, 'right'); ?>><?php _e('Right', 'team1theme'); ?></option>
+            <option value="justify" <?php selected($text_alignment, 'justify'); ?>><?php _e('Justify', 'team1theme'); ?></option>
         </select>
     </p>
     <?php
@@ -72,6 +107,16 @@ function team1theme_save_post_meta($post_id) {
     // Save show post meta setting
     if (isset($_POST['show_post_meta'])) {
         update_post_meta($post_id, '_team1theme_show_post_meta', sanitize_text_field($_POST['show_post_meta']));
+    }
+
+    // Save heading alignment setting
+    if (isset($_POST['heading_alignment'])) {
+        update_post_meta($post_id, '_team1theme_heading_alignment', sanitize_text_field($_POST['heading_alignment']));
+    }
+
+    // Save text alignment setting
+    if (isset($_POST['text_alignment'])) {
+        update_post_meta($post_id, '_team1theme_text_alignment', sanitize_text_field($_POST['text_alignment']));
     }
 }
 add_action('save_post', 'team1theme_save_post_meta');
