f2baebe22789017cabb8381cf25f2e2f74bfd29d
Reece Kelly <reecekelly2004@outlook.com>
2025-04-09 16:17:19 +1000

    Add italics customisation

--- footer.php
+++ footer.php
@@ -42,7 +42,10 @@
             </div>
         </div>
         
-        <div class="site-info" style="<?php echo get_theme_mod('footer_text_bold', false) ? 'font-weight: bold;' : ''; ?>">
+        <div class="site-info" style="<?php 
+            echo get_theme_mod('footer_text_bold', false) ? 'font-weight: bold;' : ''; 
+            echo get_theme_mod('footer_text_italic', false) ? 'font-style: italic;' : ''; 
+        ?>">
             <?php echo wp_kses_post(get_theme_mod('footer_text', 'Copyright © ' . date('Y') . ' ' . get_bloginfo('name'))); ?>
         </div><!-- .site-info -->
     </footer><!-- #colophon -->
